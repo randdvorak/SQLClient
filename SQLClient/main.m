2f28060544b7c17870f03a5a0b6cb41b5962bbc2
Rand Dvorak <randdvorak@gmail.com>
2018-05-15 16:18:58 -0400

    Initial Commit

---
+++ SQLClient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SQLClient
+//
+//  Created by Rand on 5/15/18.
+//  Copyright © 2018 Rand Dvorak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
